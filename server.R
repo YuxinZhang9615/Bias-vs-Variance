389a3dbbb18ab886285d76ed7eaf1878e5deba2f
YuxinZhang9615 <YuxinZhang9615@gmail.com>
2017-06-11 21:21:23 -0400

    To do: reset, instruction

--- server.R
+++ server.R
@@ -5,13 +5,15 @@ shinyServer(function(input, output,session) {
   var2 <- reactiveValues(x = NULL, y = NULL)
   var3 <- reactiveValues(x = NULL, y = NULL)
   var4 <- reactiveValues(x = NULL, y = NULL)
+  
 
+#Create function for ploting the target  
   plotTarget = function(x,y){
     #Get image
     isolate(ima <- readPNG("t6.png"))
     
     #Set up the plot area
-    isolate(plot(x=-5:5,ylim=c(-5,5),xlim=c(-5,5),type='p',xlab = ''))
+    isolate(plot(x=-5:5,ylim=c(-5,5),xlim=c(-5,5),type='p',xlab = '',ylab = ''))
     
     #Get the plot information so the image will fill the plot box, and draw it
     isolate(lim <- par())
@@ -20,20 +22,29 @@ shinyServer(function(input, output,session) {
     lines(x,y, xlim = c(-5,5), ylim = c(-5,5),type = 'p', pch = 20)
   }
   
+#Create function for ploting the bias plot  
   plotA = function(x,y){
     plot(x = -3:3,xlim = c(-6,6), type = "n", xlab = "", ylab = "", main = "Bias")
     box(col = "red")
     abline(v=0,col = "red")
     abline(v = sqrt(mean(x)^2 + mean(y)^2))
   }
+
+#Create function for ploting the reliability plot  
+  plotB = function(x,y){
+    #Density plot of the distance between each dots
+    plot(density(dist(cbind(x,y))),xlim = c(-2,12),xlab = "", main = "Reliability")
+    abline(v = mean(dist(cbind(x,y))))
+    box(col = "red")
+  }
   
-    
+##Use above three functions to code the output for tab1     
   observe({
     # Initially will be empty
     if (is.null(input$Click1)){
       return()
     }
-    
+    #Save the coordinates of clicked points as two vectors
     isolate({
       var1$x <- c(var1$x, input$Click1$x)
       var1$y <- c(var1$y, input$Click1$y)
@@ -42,7 +53,6 @@ shinyServer(function(input, output,session) {
   
   output$target1 <- renderPlot({
     plotTarget(var1$x,var1$y)
-    
   },height = 400, width = 400)
   
   output$plot1a <- renderPlot({
@@ -50,8 +60,26 @@ shinyServer(function(input, output,session) {
   },height = 400, width = 400)
   
   output$plot1b <- renderPlot({
-    plot(density(dist(cbind(var1$x,var1$y))),xlim = c(-2,12),xlab = "", main = "Reliability")
+    plotB(var1$x,var1$y)
   },height = 400, width = 400)
+  
+  #Distance between the center of target(population) and the center of the cloud of dots(ave. of samples)
+  output$bias1 <- renderText({
+    print(sqrt(mean(var1$x)^2 + mean(var1$y)^2))
+  })
+  
+  #Average distance between each dots
+  output$reliability1 <- renderText({
+    print(mean(dist(cbind(var1$x,var1$y))))
+  })
+  
+  output$answer1 <- renderText({
+    #When the measurement of bias > 2 and of variance >2.5, it is considered as correct
+    if ((sqrt(mean(var1$x)^2 + mean(var1$y)^2) > 2) & 
+        (mean(dist(cbind(var1$x,var1$y))) > 2.5)){
+      print("Correct!")
+    }else{print("Wrong! Please try again.")}
+  })
   
 
 ###Repeat three times for the other three tabs
@@ -74,8 +102,25 @@ shinyServer(function(input, output,session) {
   },height = 400, width = 400)
   
   output$plot2b <- renderPlot({
-    plot(density(dist(cbind(var2$x,var2$y))),xlim = c(-2,12),xlab = "", main = "Reliability")
+    plotB(var2$x,var2$y)
   },height = 400, width = 400)
+  
+  output$bias2 <- renderText({
+    print(sqrt(mean(var2$x)^2 + mean(var2$y)^2))
+  })
+  
+  output$reliability2 <- renderText({
+    print(mean(dist(cbind(var2$x,var2$y))))
+  })
+  
+  output$answer2 <- renderText({
+    #When the measurement of bias > 2 and of variance < 2, it is considered as correct
+    if ((sqrt(mean(var2$x)^2 + mean(var2$y)^2) > 2) & 
+        (mean(dist(cbind(var2$x,var2$y))) < 2)){
+      print("Correct!")
+    }else{print("Wrong! Please try again.")}
+  })
+  
   
   observe({
     if (is.null(input$Click3)){
@@ -90,12 +135,31 @@ shinyServer(function(input, output,session) {
   output$target3 <- renderPlot({
     plotTarget(var3$x,var3$y)
   },height = 400, width = 400)
+  
   output$plot3a <- renderPlot({
     plotA(var3$x,var3$y)
   },height = 400, width = 400)
+  
   output$plot3b <- renderPlot({
-    plot(density(dist(cbind(var3$x,var3$y))),xlim = c(-2,12),xlab = "", main = "Reliability")
+    plotB(var3$x,var3$y)
   },height = 400, width = 400)
+  
+  output$bias3 <- renderText({
+    print(sqrt(mean(var3$x)^2 + mean(var3$y)^2))
+  })
+  
+  output$reliability3 <- renderText({
+    print(mean(dist(cbind(var3$x,var3$y))))
+  })
+  
+  output$answer3 <- renderText({
+    #When the measurement of bias < 1 and of variance > 2.5, it is considered as correct
+    if ((sqrt(mean(var3$x)^2 + mean(var3$y)^2) < 1) & 
+        (mean(dist(cbind(var3$x,var3$y))) > 2.5)){
+      print("Correct!")
+    }else{print("Wrong! Please try again.")}
+  })
+  
   
   observe({
     if (is.null(input$Click4)){
@@ -110,12 +174,31 @@ shinyServer(function(input, output,session) {
   output$target4 <- renderPlot({
     plotTarget(var4$x,var4$y)
   },height = 400, width = 400)
+  
   output$plot4a <- renderPlot({
     plotA(var4$x,var4$y)
   },height = 400, width = 400)
+  
   output$plot4b <- renderPlot({
-    plot(density(dist(cbind(var4$x,var4$y))),xlim = c(-2,12),xlab = "", main = "Reliability")
+    plotB(var4$x,var4$y)
   },height = 400, width = 400)
+  
+  output$bias4 <- renderText({
+    print(sqrt(mean(var4$x)^2 + mean(var4$y)^2))
+  })
+  
+  output$reliability4 <- renderText({
+    print(mean(dist(cbind(var4$x,var4$y))))
+  })
+  
+  output$answer4 <- renderText({
+    #When the measurement of bias < 1 and of variance < 2, it is considered as correct
+    if ((sqrt(mean(var4$x)^2 + mean(var4$y)^2) < 1) & 
+        (mean(dist(cbind(var4$x,var4$y))) < 2)){
+      print("Correct!")
+    }else{print("Wrong! Please try again.")}
+  })
+  
  
 
     
--- ui.R
+++ ui.R
@@ -3,6 +3,7 @@ library(png)
 
 shinyUI(navbarPage("Bias + Variance",
                    tabPanel("Large Bias + large variance",
+                            wellPanel(h2("Instructions will be written here...")),hr(),
                             fluidRow(
                               column(4,plotOutput("target1", click = 'Click1')),
                               column(8, 
@@ -10,17 +11,24 @@ shinyUI(navbarPage("Bias + Variance",
                                        condition = 'input.submit1 != 0',
                                        fluidRow(
                                          column(5,plotOutput("plot1a")),
-                                         column(5,plotOutput("plot1b")))))),
+                                         column(5,plotOutput("plot1b"))),
+                                       fluidRow(
+                                         column(4,offset = 2,h4(textOutput("bias1"))),
+                                         column(4,offset = 2,h4(textOutput("reliability1")))
+                                         )))),
                             fluidRow(
                               column(4, offset = 4, actionButton("submit1","Submit"))
                             ),
                             fluidRow(
                               br(),
                               hr(),
+                              conditionalPanel("input.submit1 != 0",
+                                               h1(textOutput("answer1"))),
                               wellPanel("Instructions will be written here ...")
                             )
                    ),
                    tabPanel("Large Bias + small varianve",
+                            wellPanel(h2("Instructions will be written here...")),hr(),
                             fluidRow(
                               column(4,plotOutput("target2", click = 'Click2')),
                               column(8,
@@ -28,18 +36,25 @@ shinyUI(navbarPage("Bias + Variance",
                                        condition = 'input.submit2 != 0',
                                        fluidRow(
                                          column(5,plotOutput("plot2a")),
-                                         column(5,plotOutput("plot2b")))))),
+                                         column(5,plotOutput("plot2b"))),
+                                       fluidRow(
+                                         column(4,offset = 2,h4(textOutput("bias2"))),
+                                         column(4,offset = 2,h4(textOutput("reliability2")))
+                                       )))),
                             fluidRow(
                               column(4, offset = 4, actionButton("submit2","Submit"))
                             ),
                             fluidRow(
                               br(),
                               hr(),
+                              conditionalPanel("input.submit2 != 0",
+                                               h1(textOutput("answer2"))),
                               wellPanel("Instructions will be written here ...")
                             )
                    ),
                    
                    tabPanel("No Bias + large variance",
+                            wellPanel(h2("Instructions will be written here...")),hr(),
                             fluidRow(
                               column(4,plotOutput("target3", click = 'Click3')),
                               column(8,
@@ -47,18 +62,25 @@ shinyUI(navbarPage("Bias + Variance",
                                        condition = 'input.submit3 != 0',
                                        fluidRow(
                                          column(5,plotOutput("plot3a")),
-                                         column(5,plotOutput("plot3b")))))),
+                                         column(5,plotOutput("plot3b"))),
+                                       fluidRow(
+                                         column(4,offset = 2,h4(textOutput("bias3"))),
+                                         column(4,offset = 2,h4(textOutput("reliability3")))
+                                       )))),
                             fluidRow(
                               column(4, offset = 4, actionButton("submit3","Submit"))
                             ),
                             fluidRow(
                               br(),
                               hr(),
+                              conditionalPanel("input.submit3 != 0",
+                                               h1(textOutput("answer3"))),
                               wellPanel("Instructions will be written here ...")
                             )
                    ),
                    
                    tabPanel("No Bias + small variance",
+                            wellPanel(h2("Instructions will be written here...")),hr(),
                             fluidRow(
                               column(4,plotOutput("target4", click = 'Click4')),
                               column(8,
@@ -66,13 +88,19 @@ shinyUI(navbarPage("Bias + Variance",
                                        condition = 'input.submit4 != 0',
                                        fluidRow(
                                          column(5,plotOutput("plot4a")),
-                                         column(5,plotOutput("plot4b")))))),
+                                         column(5,plotOutput("plot4b"))),
+                                       fluidRow(
+                                         column(4,offset = 2,h4(textOutput("bias4"))),
+                                         column(4,offset = 2,h4(textOutput("reliability4")))
+                                       )))),
                             fluidRow(
                               column(4, offset = 4, actionButton("submit4","Submit"))
                             ),
                             fluidRow(
                               br(),
                               hr(),
+                              conditionalPanel("input.submit4 != 0",
+                                               h1(textOutput("answer4"))),
                               wellPanel("Instructions will be written here ...")
                             )
                    )
